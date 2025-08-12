f25aa6371b9fe639bc97a4bd937de34ae73fdcfa
Davi Oliveira <davi.ol.santos@gmail.com>
2025-08-12 10:13:40 -0300

    criando card de download de termo de compromisso

--- src/app/home/components/main-section.tsx
+++ src/app/home/components/main-section.tsx
@@ -1,22 +1,42 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 
-export function MainSection(){
-  return(
+const cards = [
+  {
+    title: 'Upload vídeos da 2ª fase',
+    description: 'Faça o upload dos vídeos dos ganhadores referentes à segunda fase. Lembramos que é permitido enviar no máximo 2 vídeos na área destinada para o envio.',
+    href: 'https://www.google.com/',
+    button: 'Enviar vídeo',
+  },
+  {
+    title: 'Termo de Compromisso e Uso de Imagem',
+    description: 'Faça o download do Termo de Compromisso e, após assinado e datado por todos os responsáveis, faça o upload do documento na área destinada para o envio.',
+    href: 'DCF-Termo de Compromisso com a Integridade Acadêmica.pdf',
+    button: 'Fazer download',
+  },
+]
+
+export function MainSection() {
+  return (
     <main className="max-w-300 w-4/5 mx-auto py-10">
       <section className="grid grid-cols-4">
-        <Card className="max-w-70">
-          <CardContent className="flex flex-col justify-center items-center gap-5">
-            <CardTitle>
-              Upload vídeos da 2ª fase
-            </CardTitle>
-            <Button>
-              <a href="https://www.google.com/" target="_blank"> 
-                Enviar vídeo
-              </a>
-            </Button>
-          </CardContent>
-        </Card>
+        {cards.map((card) => (
+          <Card className="max-w-70" key={card.title}>
+            <CardContent className="flex flex-col justify-between items-center h-full gap-5">
+              <CardTitle className="text-center leading-5">
+                {card.title}
+              </CardTitle>
+              <CardDescription className="text-center">
+                {card.description}
+              </CardDescription>
+              <Button>
+                <a href={card.href} download>
+                  {card.button}
+                </a>
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
       </section>
     </main>
   )
