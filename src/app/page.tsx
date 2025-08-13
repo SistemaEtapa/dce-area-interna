e9f56cc24681e4a1b5181c9fb40049b792c18c8f
SistemaEtapa <comunicacao@sistemaetapa.com.br>
2025-08-13 08:57:29 -0300

    colocando busca por e-mail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,31 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Logo from '../../public/decifra-logo.png'
 import Image from "next/image";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
+  const router = useRouter()
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    try {
+      const response = await axios.get(`https://decifra-3a9c8228edcb.herokuapp.com/professor/find/${email}`);
+      
+      sessionStorage.setItem("email", email);
+      console.log(response);
+      
+      if(response.status === 201) {
+        router.push("/home")
+      } else {
+        alert("Email não encontrado")
+      }
+      
+    } catch (error) {
+      console.log(error);
+      alert("Email não encontrado")
+    }
   };
 
   return (
@@ -29,13 +47,6 @@ export default function Home() {
                 placeholder="E-mail"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <Input
-                placeholder="Senha"
-                type="password"
-                value={senha}
-                onChange={(e) => setSenha(e.target.value)}
                 required
               />
             </div>
