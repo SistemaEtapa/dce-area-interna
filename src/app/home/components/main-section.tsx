955cf595b752276081d70dba7989d617fdf1a425
Sistema Etapa <comunicacao@sistemaetapa.com.br>
2025-08-26 16:22:04 -0300

    feat: adicionando botão para envio de documentos das pontuações

--- src/app/home/components/main-section.tsx
+++ src/app/home/components/main-section.tsx
@@ -9,12 +9,44 @@ export function MainSection() {
         <Card className="max-w-70">
           <CardContent className="flex flex-col justify-between items-center h-full gap-5">
             <CardTitle className="text-center leading-5">
+              Documentos da 1ª fase
+            </CardTitle>
+            <CardDescription className="text-center">
+              Confira as instruções no vídeo.
+
+              Abaixo, você terá três botões de acesso às perguntas, do gabarito com a pontuação referente à cada resposta e do link para enviar a pontuação das equipes <br /><br />
+
+              <i>(Lembre-se que o gabarito deve ser acessado apenas pelos professores)</i>
+            </CardDescription>
+            <div className="text-center auto space-y-2">
+              <Button className="w-full">
+                <a className="flex gap-2" href="dcf-questões-fase-1.pdf" target="_blank" download>
+                  <Download /> Fazer download das questões
+                </a>
+              </Button>
+              <Button className="w-full">
+                <a className="flex gap-2" href="dcf-gabarito-fase-1.pdf" target="_blank" download>
+                  <Download /> Fazer download do gabarito
+                </a>
+              </Button>
+              <Button className="w-full">
+                <a className="flex gap-2" href="https://www.dropbox.com/request/35vX8mRdb1snBnsq0pyy" target="_blank" download>
+                  <Upload /> Enviar pontuações
+                </a>
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card className="max-w-70">
+          <CardContent className="flex flex-col justify-between items-center h-full gap-5">
+            <CardTitle className="text-center leading-5">
               Upload vídeos da 2ª fase
             </CardTitle>
             <CardDescription className="text-center">
               Faça o upload dos vídeos dos ganhadores referentes à segunda fase. Lembramos que é permitido enviar no máximo 2 vídeos na área destinada para o envio.
             </CardDescription>
-            <Button>
+            <Button className="w-full">
               <a className="flex gap-2" href="https://www.dropbox.com/request/I2bwMX0UXHhvE9m3S2Hn" target="_blank" download>
                 <Upload /> Enviar vídeo
               </a>
@@ -31,12 +63,12 @@ export function MainSection() {
               Faça o download do Termo de Compromisso e, após assinado e datado por todos os responsáveis, faça o upload do documento na área destinada para o envio.
             </CardDescription>
             <div className="text-center auto space-y-2">
-              <Button>
+              <Button className="w-full">
                 <a className="flex gap-2" href="DCF-Termo de Compromisso com a Integridade Acadêmica.pdf" target="_blank" download>
                   <Download /> Fazer download
                 </a>
               </Button>
-              <Button>
+              <Button className="w-full">
                 <a className="flex gap-2" href="https://www.dropbox.com/request/CCw9k6YMaGT6Vti00gG0" target="_blank">
                   <Upload /> Enviar documento
                 </a>
@@ -44,34 +76,8 @@ export function MainSection() {
             </div>
           </CardContent>
         </Card>
-
-        <Card className="max-w-70">
-          <CardContent className="flex flex-col justify-between items-center h-full gap-5">
-            <CardTitle className="text-center leading-5">
-              Documentos da 1ª fase
-            </CardTitle>
-            <CardDescription className="text-center">
-              Confira as instruções no vídeo.
-
-              Abaixo, você terá dois botões de acesso às perguntas e o gabarito com a pontuação referente à cada resposta 📜 <br /><br />
-
-              <i>(Lembre-se que o gabarito deve ser acessado apenas pelos professores)</i>
-            </CardDescription>
-            <div className="text-center auto space-y-2">
-              <Button>
-                <a className="flex gap-2" href="dcf-questões-fase-1.pdf" target="_blank" download>
-                  <Download /> Fazer download das questões
-                </a>
-              </Button>
-              <Button>
-                <a className="flex gap-2" href="dcf-gabarito-fase-1.pdf" target="_blank" download>
-                  <Download /> Fazer download do gabarito
-                </a>
-              </Button>
-            </div>
-          </CardContent>
-        </Card>
       </section>
+
       <section className="flex justify-start gap-5">
         <div className="relative w-4/5 pb-80 md:w-80 md:pb-250 overflow-hidden mx-auto">
           <iframe
